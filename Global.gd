bda21255666b863c8447730cd6cefa97ecc416e7
Layetta <aplawill@iu.edu>
2023-04-08 12:38:46 -0400

    Game Progression

--- Global.gd
+++ Global.gd
@@ -7,12 +7,17 @@ func _ready():
 
 func _unhandled_input(_event):
 	if Input.is_action_just_pressed("menu"):
+		print("escape/menu pressed")
 		if menu == null:
 			menu = get_node_or_null("/root/Game/UI/Menu")
+			print("menu now exists")
 		if menu != null:
+			print("menu isnt null")
 			if not menu.visible:
 				get_tree().paused = true
 				menu.show()
+				print("menu is visible")
 			else:
 				get_tree().paused = false
+				print("menu is invisible")
 				menu.hide()
